2c0bba159a6e3ceb976a93bdd78ca1bf7e15e20b
Stefan Agapie <stefanagapie@gmail.com>
2015-04-26 10:40:59 -0400

    Update PlumbingSupplyItem.swift

--- Basic Swift Networking/PlumbingSupplyItem.swift
+++ Basic Swift Networking/PlumbingSupplyItem.swift
@@ -5,6 +5,21 @@
 //  Created by Stefan Agapie on 10/7/14.
 //  Copyright (c) 2014 aGupieWare. All rights reserved.
 //
+//  This file is part of Basic Swift Networking.
+//
+//  Basic Swift Networking is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Basic Swift Networking is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Basic Swift Networking.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 import UIKit
 
