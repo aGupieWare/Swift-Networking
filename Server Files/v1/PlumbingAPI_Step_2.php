4f2211a5ae2ebffd6911efb2ed42b80843c96a0e
Stefan Agapie <stefanagapie@gmail.com>
2015-04-27 08:23:20 -0400

    Update PlumbingAPI_Step_2.php

--- Server Files/v1/PlumbingAPI_Step_2.php
+++ Server Files/v1/PlumbingAPI_Step_2.php
@@ -10,6 +10,20 @@
     Output: A formatted HTTP response
  
     Author: aGupieWare (This script is an adaptation of Mark Roland Demo API)
+    
+    This file is part of Basic Swift Networking.
+    
+    Basic Swift Networking is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    Basic Swift Networking is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+  
+      You should have received a copy of the GNU General Public License
+      along with Basic Swift Networking.  If not, see <http://www.gnu.org/licenses/>.
 */
  
 // ** Initialize variables and functions ** //
